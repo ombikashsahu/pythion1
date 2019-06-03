14c8ea9090022c845843da39f2f08d2b57a3560a
Om Bikash Sahu <42239244+ombikashsahu@users.noreply.github.com>
2019-06-03 16:33:20 +0530

    Add files via upload

---
+++ summer python week 1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# 2-1. Simple Message: Store a message in a variable, and then print that message .
+best_friend ="Symphony" 
+print(best_friend)
+
+
+# In[6]:
+
+
+# 2-2. Simple Messages: Store a message in a variable, and print that message . Then change the value of your variable to a new message, and print the new message .
+my_laptop = "acer"
+print(my_laptop)
+my_laptop = "apple"
+print(my_laptop)
+
+
+# In[7]:
+
+
+# 2-3. Personal Message: Store a person’s name in a variable, and print a message to that person 
+person_name = "Rohit"
+print("Hey!",person_name,"welcome to you in python this summer.")
+
+
+# In[11]:
+
+
+# 2-4. Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase .
+user_name = "Evan"
+print(user_name.lower())
+print(user_name.upper())
+print(user_name.title())
+
+
+# In[14]:
+
+
+# 2-5. Famous Quote: Find a quote from a famous person you admire . Print the quote and the name of its author
+print('Dennis Kimbro once said, "Life is only 10% what happens to us rest all 90% is how we react it."')
+
+
+# In[17]:
+
+
+# 2-6. Famous Quote 2: Repeat Exercise 2-5, but this time store the famous person’s name in a variable called famous_person . Then compose your message and store it in a new variable called message . Print your message .
+famous_person ="Dennis Kimbr -"
+messege = "Life is only 10% what happens to us rest all 90% is how we react it."
+print(famous_person,messege)
+
+
+# In[19]:
+
+
+# 2-8. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8 .
+print(2+6)
+print(16-8)
+print(2*4)
+print(24/3)
+
+
+# In[ ]:
+
+
+favourite_number = (6)
+
